04a1d3513cfa5bc7f69bc88b4ad9b4d6b2d9e2f9
Kodrillar <ipadeoladavid112@gmail.com>
2022-09-13 15:21:11 +0100

    loading annotated region helper widget

--- client/lib/src/widgets/loading_indicator.dart
+++ client/lib/src/widgets/loading_indicator.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intola/src/utils/constant.dart';
+import 'package:intola/src/widgets/annotated_region.dart';
 
 class LoadingIndicator extends StatelessWidget {
   const LoadingIndicator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle.light,
+    return SystemUIOverlayAnnotatedRegion(
+      systemUiOverlayStyle: SystemUiOverlayStyle.light,
       child: Container(
         color: kDarkBlue,
         child: const Center(
